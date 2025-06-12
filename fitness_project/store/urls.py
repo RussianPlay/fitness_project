d9d0a0b314f128f61aeadfaf8d8e295d4d6052ea
RussianPlay <ahmadullin32@mail.ru>
2025-06-12 13:47:29 +0300

    comm_11

---
+++ fitness_project/store/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path, include, re_path
+from .views import index, product_tab, cart, index_with_order, cart_detail, cart_add, cart_remove, cart_decrease, payment
+urlpatterns = [
+    path('page/', index, name='home'),
+    path('page/<int:page>', index, name='move_to_page'),
+    path('page/<int:page>/catalog/', index_with_order),  # Без сортировки
+    path('page/<int:page>/catalog/<str:sort_command>/', index_with_order),  # С сортировкой
+    path('product/<int:product_id>/', product_tab),
+    path('payment/', payment),
+
+    path('cart/', cart_detail, name='cart_detail'),
+    path('cart/add/product/<int:product_id>/', cart_add, name='cart_add'),
+    path('cart/decrease/product/<int:product_id>/', cart_decrease, name='cart_decrease'),
+    path('cart/remove/product/<int:product_id>/', cart_remove, name='cart_remove'),
+]
+#'products/<int:productid>/
